f7475dfecaafdc220fb79f10b064ba4eb2de9b59
Gabriel Trajano <gabriel-trajano@outlook.com>
2022-06-14 16:02:44 -0300

    Testado a inclusão de produto no carrinho.

--- src/test/java/homepage/HomePageTests.java
+++ src/test/java/homepage/HomePageTests.java
@@ -3,12 +3,15 @@ package homepage;
 import base.BaseTests;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
+import pages.ModalProdutoPage;
 import pages.ProdutoPage;
 
+import java.util.List;
 import java.util.Locale;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class HomePageTests extends BaseTests {
@@ -25,25 +28,25 @@ public class HomePageTests extends BaseTests {
         assertThat(produtosNoCarrinho, is(0));
     }
 
+    ProdutoPage produtoPage;
+    String nomeProduto_ProdutoPage;
     @Test
     public void testValidarDetalhesDoProduto_DescricaoEValorIguais(){
         int indice = 0;
         String nomeProduto_HomePage = homePage.obterNomeProduto(indice);
         String precoProduto_HomePage = homePage.obterPrecoProduto(indice);
 
-        ProdutoPage produtoPage = homePage.clicarProduto(indice);
+        produtoPage = homePage.clicarProduto(indice);
 
-        String nomeProduto_ProdutoPage = produtoPage.obterNomeDoProduto();
+        nomeProduto_ProdutoPage = produtoPage.obterNomeDoProduto();
         String precoProduto_ProdutoPage = produtoPage.obterPrecoDoProduto();
-
-        System.out.println(nomeProduto_ProdutoPage);
-        System.out.println(precoProduto_ProdutoPage);
 
         assertThat(nomeProduto_HomePage.toLowerCase(), is(nomeProduto_ProdutoPage.toLowerCase()));
         assertThat(precoProduto_HomePage.toLowerCase(), is(precoProduto_ProdutoPage.toLowerCase()));
 
     }
 
+    LoginPage loginPage;
     @Test
     public void testLoginComSucesso_UsuarioLogado(){
         // Clicar no botão Sign In na Homepage.
@@ -58,6 +61,58 @@ public class HomePageTests extends BaseTests {
 
         // Validar se o usuário está logado.
         assertThat(homePage.estaLogado("Marcelo Bittencourt"), is(true));
+        carregarPaginaInicial();
+    }
 
+    @Test
+    public void incluirProdutoNoCarrinho_ProdutoIncluidoComSucesso(){
+        String tamanhoProduto = "M";
+        String corProduto = "Black";
+        int quantidadeProduto = 2;
+
+        // Pré-condição: Usuário logado.
+        if(!homePage.estaLogado(("Marcelo Bittencourt"))){
+            testLoginComSucesso_UsuarioLogado();
+        }
+        // Teste:
+        // Selecionar produto
+        testValidarDetalhesDoProduto_DescricaoEValorIguais();
+
+        // Selecionar tamanho
+        List<String> listaOpcoes = produtoPage.obterOpcoesSelecionadas();
+
+        produtoPage.selecionarOpcaoDropdown(tamanhoProduto);
+
+        listaOpcoes = produtoPage.obterOpcoesSelecionadas();
+
+        // Selecionar cor
+        produtoPage.selecionarCorPreta();
+
+        // Selecionar quantidade
+        produtoPage.alterarQuantidade(quantidadeProduto);
+
+        // Adicionar no carrinho
+        ModalProdutoPage modalProdutoPage = produtoPage.clicarBotaoAddToCart();
+
+        // Validações
+        assertTrue(modalProdutoPage.obterMensagemProdutoAdicionado().endsWith("Product successfully added to your shopping cart"));
+
+        assertThat(modalProdutoPage.obterTamanhoProduto(), is(tamanhoProduto));
+        assertThat(modalProdutoPage.obterCorProduto(), is(corProduto));
+        assertThat(modalProdutoPage.obterQuantidadeProduto(), is(Integer.toString(quantidadeProduto)));
+
+        String precoProdutoString = modalProdutoPage.obterPrecoProduto();
+        precoProdutoString = precoProdutoString.replace("$", "");
+        Double precoProduto = Double.parseDouble(precoProdutoString);
+
+        String subtotalString = modalProdutoPage.obterSubtotal();
+        subtotalString = subtotalString.replace("$", "");
+        Double subtotal = Double.parseDouble(subtotalString);
+
+        Double subtotalCalculado = quantidadeProduto * precoProduto;
+
+        assertThat(subtotal, is(subtotalCalculado));
+
+        assertThat(modalProdutoPage.obterDescricaoProduto().toLowerCase(), is(nomeProduto_ProdutoPage.toLowerCase()));
     }
 }
